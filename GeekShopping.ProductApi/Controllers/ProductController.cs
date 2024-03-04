fbcdf5fabd7b08aaa466a9a633b80b3f12e55080
Sandro Goncalves <gonsandroo@gmail.com>
2024-03-04 11:11:22 -0300

    Add project to repository

---
+++ GeekShopping.ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using GeekShopping.ProductApi.Data.ValueObj;
+using GeekShopping.ProductApi.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> CreateProduct(ProductVO product)
+        {
+            if (product == null) return BadRequest();
+            var createProduct = await _repository.Create(product);
+
+            return Ok(createProduct);
+        } 
+        
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> UpdateProduct(ProductVO product)
+        {
+            if (product == null) return BadRequest();
+            var UpdateProduct = await _repository.Update(product);
+
+            return Ok(UpdateProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(long id)
+        {
+            var DeleteProductStatus = await _repository.Delete(id);
+            if(!DeleteProductStatus) return BadRequest();
+
+            return Ok(DeleteProductStatus);
+        }
+
+    }
+}
---
+++ GeekShopping.ProductApi/Repository/Interface/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.ProductApi.Data.ValueObj;
+
+namespace GeekShopping.ProductApi.Repository.Interface
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
+    }
+}
