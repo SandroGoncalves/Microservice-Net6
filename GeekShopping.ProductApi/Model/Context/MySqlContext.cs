d8b2cdf4828791853902c66f8aad4072799d3c2e
Sandro Goncalves <gonsandroo@gmail.com>
2024-03-04 11:17:44 -0300

    moving img to outside project

--- GeekShopping.ProductApi/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductApi/Model/Context/MySqlContext.cs
@@ -17,7 +17,7 @@ namespace GeekShopping.ProductApi.Model.Context
                 Name = "Camiseta No Internet",
                 Price = new decimal(69.9),
                 Description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
-                ImageUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/2_no_internet.jpg?raw=true",
+                ImageUrl = "https://github.com/SandroGoncalves/Microservice-Net6/blob/master/GeekShopping.ProductApi/ShoppingImg/2_no_internet.jpg?raw=true",
                 CategoryName = "T-shirt"
             });
             modelBuilder.Entity<Product>().HasData(new Product
